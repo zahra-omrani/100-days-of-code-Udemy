890ae78678bc3d35748bc3a61fdae08a209bc44f
elahe <z.omrani@studenti.unipi.it>
2025-02-18 15:35:47 +0100

    started day 15

---
+++ Day 15/main.py
@@ -0,0 +1,50 @@
+
+
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# TODO: Prompt user by asking “ What would you like? (espresso/latte/cappuccino):​ ” 
+
+# TODO: Turn off the Coffee Machine by entering “ off​ ” to the prompt. 
+
+# TODO: Print report. 
+
+# TODO: Check resources sufficient? 
+
+# TODO: Process coins. 
+
+# TODO: Check transaction successful? 
+
+# TODO: Make Coffee. 
+
+# TODO - todo note
