17d99a20efd6730b0ad815bf69ac819035d17adb
elahe <z.omrani@studenti.unipi.it>
2025-02-19 10:41:28 +0100

    started the coffe mechine project

--- Day 15/main.py
+++ Day 15/main.py
@@ -1,6 +1,9 @@
 
 
 
+import sys
+
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -33,16 +36,39 @@ resources = {
     "coffee": 100,
 }
 
-# TODO: Prompt user by asking “ What would you like? (espresso/latte/cappuccino):​ ” 
-
 # TODO: Turn off the Coffee Machine by entering “ off​ ” to the prompt. 
+def turn_off(user_input):
+    if user_input == 'off':
+        sys.exit("User asked to turn off the coffe mechine!")
+    
 
 # TODO: Print report. 
+def print_report(user_input):
+    if user_input == 'report':
+        print(
+            f"Water: {resources['water']}\n"
+            f"Milk: {resources['milk']}\n"
+            f"Coffee: {resources['coffee']}\n"
+            "Money: 000"
+        )
 
+
+# TODO: Prompt user by asking “ What would you like? (espresso/latte/cappuccino):​ ” 
+order = input("What would you like? (espresso/latte/cappiccino): ")
+
+print_report(order)
+turn_off(order)
 # TODO: Check resources sufficient? 
-
+def resource_sufficient():
+    if resources['water'] < 50 :
+        print("Sorry there is not enough water!")
+    if resources ["milk"] < 100: 
+        print("Sorry there is not enough milk")
+    if resources ["coffee"] < 18:
+        print("Sorry there is not enough coffee")
 # TODO: Process coins. 
-
+def process_coin():
+  
 # TODO: Check transaction successful? 
 
 # TODO: Make Coffee. 
