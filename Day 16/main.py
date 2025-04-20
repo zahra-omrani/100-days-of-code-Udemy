68f3573dba82b7998a9fe74d2acf51f373375986
elahe <z.omrani@studenti.unipi.it>
2025-04-20 10:31:30 +0200

    oop coffee machine

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,6 +1,14 @@
-from prettytable import PrettyTable
-table = PrettyTable()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-table.add_column("Name","")
-table.add_column("Type","")
-print(table)
+my_coffe_maker = CoffeeMaker()
+my_money_machine = MoneyMachine()
+
+user_order = input("What would you like? (espresso/latte/cappuccino/):")
+recieved_money = my_money_machine.process_coins()
+print(recieved_money)
+if user_order == 'report':
+   my_coffe_maker.report()
+elif my_coffe_maker.is_resource_sufficient(user_order):
+   my_coffe_maker.make_coffee()
