29940f7db861283e5f7e0a4e8a5f5e60a1070c4f
elahe <z.omrani@studenti.unipi.it>
2025-04-21 21:35:45 +0200

    worked on the coffee mechine

--- Day 16/main.py
+++ Day 16/main.py
@@ -10,5 +10,9 @@ recieved_money = my_money_machine.process_coins()
 print(recieved_money)
 if user_order == 'report':
    my_coffe_maker.report()
+
+elif my_money_machine.make_payment(recieved_money) == False:
+   exit()  # Exit the program if payment fails
+
 elif my_coffe_maker.is_resource_sufficient(user_order):
-   my_coffe_maker.make_coffee()
+   my_coffe_maker.make_coffee(user_order)
