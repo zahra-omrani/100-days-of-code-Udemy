64b12dcba34bf5bca5b580ee85a46d8ee710fb34
elahe <z.omrani@studenti.unipi.it>
2025-04-22 21:21:46 +0200

    used the menue class instead of the string for the order

--- Day 16/main.py
+++ Day 16/main.py
@@ -4,9 +4,14 @@ from money_machine import MoneyMachine
 
 my_coffe_maker = CoffeeMaker()
 my_money_machine = MoneyMachine()
+my_menue = Menu()
 
 user_order = input("What would you like? (espresso/latte/cappuccino/):")
 recieved_money = my_money_machine.process_coins()
+
+order_menue = my_menue.find_drink(user_order)
+print(order_menue)
+
 print(recieved_money)
 if user_order == 'report':
    my_coffe_maker.report()
@@ -14,5 +19,5 @@ if user_order == 'report':
 elif my_money_machine.make_payment(recieved_money) == False:
    exit()  # Exit the program if payment fails
 
-elif my_coffe_maker.is_resource_sufficient(user_order):
-   my_coffe_maker.make_coffee(user_order)
+elif my_coffe_maker.is_resource_sufficient(order_menue):
+   my_coffe_maker.make_coffee(order_menue)
