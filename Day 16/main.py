f01790b29683898e66d34cbb410345ead8a0634a
elahe <z.omrani@studenti.unipi.it>
2025-04-25 09:04:33 +0200

    finished day 16

--- Day 16/main.py
+++ Day 16/main.py
@@ -6,18 +6,17 @@ my_coffe_maker = CoffeeMaker()
 my_money_machine = MoneyMachine()
 my_menue = Menu()
 
-user_order = input("What would you like? (espresso/latte/cappuccino/):")
-recieved_money = my_money_machine.process_coins()
+is_on = True
 
-order_menue = my_menue.find_drink(user_order)
-print(order_menue)
+while is_on:
+    options = my_menue.get_items()
+    choice = input(f'what do you like ({options})')
 
-print(recieved_money)
-if user_order == 'report':
-   my_coffe_maker.report()
-
-elif my_money_machine.make_payment(recieved_money) == False:
-   exit()  # Exit the program if payment fails
-
-elif my_coffe_maker.is_resource_sufficient(order_menue):
-   my_coffe_maker.make_coffee(order_menue)
+    if choice == "off":
+        is_on = False
+    if choice == 'report':
+        my_coffe_maker.report()
+    else:
+      drink = my_menue.find_drink(choice)
+      if my_coffe_maker.is_resource_sufficient(drink) == True and my_money_machine.make_payment(drink.cost):
+         my_coffe_maker.make_coffee(drink)
