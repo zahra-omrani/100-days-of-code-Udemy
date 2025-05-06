aba246a7c991b6ed80aa87a62aa3e72007408cc0
elahe <z.omrani@studenti.unipi.it>
2025-05-06 19:34:28 +0200

    completed Day 17

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,14 +1,17 @@
+# main.py
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
+for item in question_data:
+    question_text = item["text"]
+    question_answer = item["answer"]
+    question_bank.append(Question(question_text, question_answer))
 
-for i in range(0,len(question_data)):
-    question_text = question_data[i]['text']
-    question_answer = question_data[i]['answer']
-    question_instance = Question(question_text,question_answer)
-    question_bank.append(question_instance)
+quiz = QuizBrain(question_bank)
 
-quiz_brain = QuizBrain(question_bank)
-quiz_brain.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz. Your final score was: {quiz.score}/{len(question_bank)}")
