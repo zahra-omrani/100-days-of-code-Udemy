076bfbf481260bd11e2c7aa411e481d7fc046a15
elahe <z.omrani@studenti.unipi.it>
2025-05-13 10:33:41 +0200

    started day 18

---
+++ Day 18/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+
+tim = Turtle ()
+tim.shape('turtle')
+tim.color('red')
+
+tim.right(90)
+tim .forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+
+screen = Screen()
+screen.exitonclick()
